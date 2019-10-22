c1cb188300d0bfb4b4be7a0e57dff10d748d19c6
Steven <steven.popick@gmail.com>
2019-10-22 17:21:56 -0500

    Image retrieval is working

--- main.cpp
+++ main.cpp
@@ -1,46 +1,61 @@
 
 #include <iostream>
-#include "deps/sleepy-discord/include/sleepy_discord/websocketpp_websocket.h"
+#include <algorithm>
 #include <string>
 #include <cpr/cpr.h>
+#include "deps/sleepy-discord/include/sleepy_discord/websocketpp_websocket.h"
+#include "deps/json/single_include/nlohmann/json.hpp"
+using json = nlohmann::json;
+using namespace std;
 
 class MyClientClass : public SleepyDiscord::DiscordClient {
 public:
+
 	using SleepyDiscord::DiscordClient::DiscordClient;
 
-	//enum Command : unsigned char {
-	//	HELP = 0
-	//};
-
-	//Command userCommand;
 	void onMessage(SleepyDiscord::Message message) override {
-		std::string msg = message.content;
+		string msg = message.content;
 		std::size_t startPos;
 
-		//std::string::npos is the not matched value ~ basically
-		if ((startPos = msg.find("[")) != std::string::npos) {
+		//string::npos is the not matched value ~ basically
+		if ((startPos = msg.find("[")) != string::npos) {
 			std::size_t endPos;
-			if ((endPos = msg.find("]")) != std::string::npos && endPos != startPos + 1) {
-				std::string cardName = msg.substr(startPos + 1, endPos - startPos - 1);
-
-				sendMessage(message.channelID, findCard(cardName));
+			if ((endPos = msg.find("]")) != string::npos && endPos != startPos + 1) {
+				string cardName = msg.substr(startPos + 1, endPos - startPos - 1);
+				string cardImage = findCard(cardName);
+				std::cout << cardImage << std::endl;
+				json embed;
+				embed = {
+					{"image", cardImage}
+				};
+				sendMessage(message.channelID, embed);
 			}
 		}
 	}
 
-	std::string findCard(std::string cardName) {
+	string findCard(string cardName) {
 		auto r = cpr::Get(cpr::Url{ "https://api.scryfall.com/cards/named" },
-			//cpr::Authentication{ "jiroscopes", "16Popste" },
-			cpr::Parameters{ { "exact", "mana drain" },{ "format", "image" } });
+			cpr::Parameters{ { "exact", cardName } });
 		r.status_code;                  // 200
 		r.header["content-type"];       // application/json; charset=utf-8
-										//return r.text;
-		std::cout << r.text;
-		return r.text;
+	
+		return extractImage(r.text);;
+	}
+
+	string extractImage(string data) {
+		json j_string = data;
+
+		json parsed = json::parse(data);
+		string imageLink = parsed["image_uris"]["large"].dump();
+		imageLink.erase(std::remove(imageLink.begin(), imageLink.end(), '\"'), imageLink.end());
+		return imageLink;
 	}
 };
 
 int main() {
-	MyClientClass client("NjM0MTg4NzczMDc2MjM4MzU4.Xafanw.0m77Mx8rPRFw5MKn4H9cOfSlDl0", 2);
+	MyClientClass client("NjM0MTg4NzczMDc2MjM4MzU4.XayN_w.mvyN5REvXXWrwqL8VtEnoGjGh0k", 2);
 	client.run();
+
+
+
 }
