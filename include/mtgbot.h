86cda3ee602b4e1e42eeec5303070240dfbed94b
Steven <steven.popick@gmail.com>
2019-11-03 19:08:38 -0600

    Breaking up my massive mess of a class

--- include/mtgbot.h
+++ include/mtgbot.h
@@ -1,6 +1,13 @@
 #pragma once
-#include <iostream>
-#include <algorithm>
-#include <string>
 #include <cpr/cpr.h>
+#include "card.h"
 #include "sleepy_discord/websocketpp_websocket.h"
+
+class mtgbot : public SleepyDiscord::DiscordClient 
+{
+public:
+	using SleepyDiscord::DiscordClient::DiscordClient;
+
+	void onMessage(SleepyDiscord::Message message);
+
+};
